6f048b199d461b0b9a064430f4a716dad149951f
mvgmb <mv3gmb@gmail.com>
2020-12-30 12:01:03 -0300

    feat(ch3): add read and write code

---
+++ ch3/2_read_write/scull.h
@@ -0,0 +1,37 @@
+#ifndef _SCULL_H_
+#define _SCULL_H_
+
+#ifndef SCULL_MAJOR
+#define SCULL_MAJOR 0 /* dynamic major by default */
+#endif
+
+#ifndef SCULL_NR_DEVS
+#define SCULL_NR_DEVS 4 /* scull0 through scull3 */
+#endif
+
+#ifndef SCULL_QUANTUM
+#define SCULL_QUANTUM 4000
+#endif
+
+#ifndef SCULL_QSET
+#define SCULL_QSET 1000
+#endif
+
+/*
+ * Representation of scull quantum sets.
+ */
+struct scull_qset {
+    void **data;
+    struct scull_qset *next;
+};
+
+struct scull_dev {
+    struct scull_qset *data;// Pointer to first quantum set
+    int quantum;            // the current quantum size
+    int qset;               // the current array size
+    unsigned long size;     // amount of data stored here
+    struct mutex lock;      // mutual exclusion semaphore
+    struct cdev cdev;       // Char device structure
+};
+
+#endif// _SCULL_H_
